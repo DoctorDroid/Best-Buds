86058664ce2a132a3acac336ac6edd61c18ca868
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2020-10-19 23:53:27 -0400

    Added framework file for peer review/revision

---
+++ api/app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+from (the request file) import *
+# TODO the request file
+import logging
+import requests
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def root():
+    pass
+
+
+@app.route('/predict', methods=['POST'])
+
+    # Requested to return Name, Type, and Description
+    json_req = request.get_json()
+    # TODO find which variables are inbound
+    input_variable1 = request_json['input_variable1']
+    input_variable2 = request_json['input_variable2']
+    input_variable3 = request_json['input_variable3']
+
+    # TODO I left this commented out because I don't believe we are required to log?
+    # url = "https://cannedmedical.herokuapp.com/recommendation/search" (wrong anyway)
+    # logging.info(url)
+    payload = {"user_id": user_id,   <<<---- the payload will be the input variables
+               "Prediction": pred}   <<<---- whatever we get from above
+    # logging.info(payload)
+    # req_log = requests.post(url, json=payload)
+    # logging.info("status_code: " + str(req_log.status_code))
+
+    # TODO ensure variables are correct for return
+    # If they want conditions treated, how should we handle that?  Should we work on combining the two, or deliver one?
+    return jsonify({'Strain':strain, 'Indica_Sativa_Hybrid':indica_sativa_hybrid, 'Description':description})
+
+if __name__ == '__main__':
+    app.run()
