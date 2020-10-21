6c131a7f09ee94aa2fb8d15654af4343864ff19a
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2020-10-21 12:27:15 -0400

    Added plan B model to app.py

--- api/app.py
+++ api/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request, jsonify
-from (the request file) import *
-# TODO the request file
-import logging
-import requests
 
+#from (the request file) import *
+# TODO the request file
+#import logging
+import requests
+import string
 
 
 app = Flask(__name__)
@@ -19,23 +20,33 @@ def root():
     # Requested to return Name, Type, and Description
     json_req = request.get_json()
     # TODO find which variables are inbound
-    effects = json_req['effects']
-    ailments = json_req['ailments']
+    # effects = json_req['effects']
+    # ailments = json_req['ailments']
     # input_variable3 = request_json['input_variable3'] *** I don't believe there is a third, but just in case.
-    # string separated by comma to list
 
-    # TODO I left this commented out because I don't believe we are required to log?
-    # url = "https://cannedmedical.herokuapp.com/recommendation/search" (wrong anyway)
-    # logging.info(url)
-    payload = {"user_id": user_id,   <<<---- the payload will be the input variables
-               "Prediction": pred}   <<<---- whatever we get from above
-    # logging.info(payload)
-    # req_log = requests.post(url, json=payload)
-    # logging.info("status_code: " + str(req_log.status_code))
+    stringData = json_req['stringData']
 
-    # TODO ensure variables are correct for return
-    # If they want conditions treated, how should we handle that?  Should we work on combining the two, or deliver one?
-    return jsonify({'Strain':strain, 'Indica_Sativa_Hybrid':indica_sativa_hybrid, 'Description':description})
+    def model1(input_string):
+            user_input = pd.DataFrame([word.strip(string.punctuation) for word in input_string.split()], 
+                                        columns=['search_string']) # string separated by comma to single column df
+            MJ= marijuana.copy()
+            MJ['fitness'] = 0 ## sets all strains initial fitness to 0
+            for i in range(len(MJ)):
+                for each_string in user_input['search_string']:
+                    if each_string in MJ['Ailment'][i]:
+                        MJ['fitness'][i] +=1
+                    if each_string in MJ['Effects_x'][i]:
+                        MJ['fitness'][i] +=1
+                    if each_string in MJ['Effects_y'][i]:
+                        MJ['fitness'][i] +=1
+            return MJ.nlargest(20, 'fitness') # as is this will return a df with the top 20 matches. May need to adjust.
+
+    df_pred = model1(StringData)
+    '''TODO for loop to extract info to return from each strain
+            strain = 
+            profile = 
+            ...other fields to return'''
+    return jsonify({'Strain':strain, 'Indica_Sativa_Hybrid':indica_sativa_hybrid, 'Description':description}) #this may need adjustments
 
 if __name__ == '__main__':
     app.run()
