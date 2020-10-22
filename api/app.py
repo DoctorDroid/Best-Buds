311d01e0f42818dbf0f3a8b32ae26a51570f92c4
jeremy-jewett <64876522+jeremy-jewett@users.noreply.github.com>
2020-10-21 23:04:57 -0400

    fixed indentations

--- api/app.py
+++ api/app.py
@@ -17,36 +17,36 @@ def root():
 
 @app.route('/predict', methods=['POST'])
 
-    # Requested to return Name, Type, and Description
-    json_req = request.get_json()
-    # TODO find which variables are inbound
-    # effects = json_req['effects']
-    # ailments = json_req['ailments']
-    # input_variable3 = request_json['input_variable3'] *** I don't believe there is a third, but just in case.
+# Requested to return Name, Type, and Description
+json_req = request.get_json()
+# TODO find which variables are inbound
+# effects = json_req['effects']
+# ailments = json_req['ailments']
+# input_variable3 = request_json['input_variable3'] *** I don't believe there is a third, but just in case.
 
-    stringData = json_req['stringData']
+stringData = json_req['stringData']
 
-    def model1(input_string):
-            user_input = pd.DataFrame([word.strip(string.punctuation) for word in input_string.split()], 
-                                        columns=['search_string']) # string separated by comma to single column df
-            MJ= marijuana.copy()
-            MJ['fitness'] = 0 ## sets all strains initial fitness to 0
-            for i in range(len(MJ)):
-                for each_string in user_input['search_string']:
-                    if each_string in MJ['Ailment'][i]:
-                        MJ['fitness'][i] +=1
-                    if each_string in MJ['Effects_x'][i]:
-                        MJ['fitness'][i] +=1
-                    if each_string in MJ['Effects_y'][i]:
-                        MJ['fitness'][i] +=1
-            return MJ.nlargest(20, 'fitness') # as is this will return a df with the top 20 matches. May need to adjust.
+def model1(input_string):
+    user_input = pd.DataFrame([word.strip(string.punctuation) for word in input_string.split()],
+                                    columns=['search_string']) # string separated by comma to single column df
+    MJ= marijuana.copy()
+    MJ['fitness'] = 0 ## sets all strains initial fitness to 0
+    for i in range(len(MJ)):
+        for each_string in user_input['search_string']:
+            if each_string in MJ['Ailment'][i]:
+                MJ['fitness'][i] +=1
+            if each_string in MJ['Effects_x'][i]:
+                MJ['fitness'][i] +=1
+            if each_string in MJ['Effects_y'][i]:
+                MJ['fitness'][i] +=1
+    return MJ.nlargest(20, 'fitness') # as is this will return a df with the top 20 matches. May need to adjust.
 
-    df_pred = model1(StringData)
-    '''TODO for loop to extract info to return from each strain
-            strain = 
-            profile = 
-            ...other fields to return'''
-    return jsonify({'Strain':strain, 'Indica_Sativa_Hybrid':indica_sativa_hybrid, 'Description':description}) #this may need adjustments
+df_pred = model1(StringData)
+'''TODO for loop to extract info to return from each strain
+        strain = 
+        profile = 
+        ...other fields to return'''
+return jsonify({'Strain':strain, 'Indica_Sativa_Hybrid':indica_sativa_hybrid, 'Description':description}) #this may need adjustments
 
 if __name__ == '__main__':
     app.run()
