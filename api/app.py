41d95d7b992b1c6b710aad82a5c01be1cfd234ca
Timothy Eakin <61610170+DoctorDroid@users.noreply.github.com>
2020-10-26 12:23:56 -0400

    added filter by type

--- api/app.py
+++ api/app.py
@@ -1,17 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 import pandas as pd
 import string
+from flask_cors import CORS, cross_origin
+import json
 
-#from (the request file) import *
-# TODO the request file
-#import logging
-#import requests
-#import string
 
 def create_app():
     app = Flask(__name__)
-    app.use(allowCrossDomain)
+    cors = CORS(app)
+    app.config['CORS_HEADERS'] = 'Content-Type'
+
     @app.route('/')
+    @cross_origin()
     def root():
         return "Honestly, Bessie? People dont care about anyone but themselves. They dont notice anything. They are never looking at whats interesting. Theyre always looking at themselves. -Kevin Wilson, Nothing to See Here "
 
@@ -19,14 +19,41 @@ def create_app():
     @app.route('/predict', methods=['GET', 'POST'])
     def model1():
         marijuana = pd.read_csv("https://raw.githubusercontent.com/Build-Week-PT-Med-Cabinet-2/DS/main/Marijuana.csv")
-        # Requested to return Name, Type, and Description
-        marijuana['Effects_x'].fillna('No Data', inplace=True)
-        marijuana['Effects_y'].fillna('No Data', inplace=True)
-        stringData = request.values['stringData']
-        user_input = pd.DataFrame([word.strip(string.punctuation) for word in stringData.split()],
-                                    columns=['search_string']) # string separated by comma to single column df
+        
+        request_json = request.get_json()
+        Type = request_json['Type']
+        Depression = request_json['Depression']
+        Inflammation = request_json['Inflammation']
+        Insomnia = request_json['Insomnia']
+        Appetite = request_json['Appetite']
+        Pain = request_json['Pain']
+        Nausea = request_json['Nausea']
+        Creative = request_json['Creative']
+        Energetic = request_json['Energetic']
+        Euphoric = request_json['Euphoric']
+        Focused = request_json['Focused']
+        Happy = request_json['Happy']
+        Hungry = request_json['Hungry']
+        Relaxed = request_json['Relaxed']
+        
+        stringData = [Type, Depression, Inflammation, Insomnia, 
+                      Appetite, Pain,Nausea, Creative, Energetic, 
+                      Euphoric,Focused, Happy, Hungry, Relaxed]
+        
+        user_input = pd.DataFrame(stringData, columns=['search_string']) # string separated by comma to single column df
+        
         MJ= marijuana.copy()
-        MJ['fitness'] = 0 ## sets all strains initial fitness to 0
+        if Type.lower() == 'sativa':
+            MJ = MJ[MJ['Type'] == 'sativa']
+        elif Type.lower() == 'indica':
+            MJ = MJ[MJ['Type'] == 'indica']
+        elif Type.lower() == 'hybrid':
+            MJ = MJ[MJ['Type'] == 'hybrid']
+        else : 
+            pass
+        
+        
+        MJ['fitness'] = 0 ## sets all strains initial 'fitness' to 0
         for i in range(len(MJ)):
             for each_string in user_input['search_string']:
                 if each_string in MJ['Ailment'][i]:
@@ -35,8 +62,10 @@ def create_app():
                     MJ['fitness'][i] +=1
                 if each_string in MJ['Effects_y'][i]:
                     MJ['fitness'][i] +=1
+        
         df = MJ.nlargest(20, 'fitness')
+        df = df.drop(columns=['id','Unnamed: 0', 'fitness'], axis=1)
         results = df.to_json(orient="records")
-        return results # as is this will return a df with the top 20 matches. May need to adjust.
-
-   return app
+        return results # this will return a df with the top 20 matches,
+                       # filtered by type, as a JSON object, without ID key. 
+    return app
